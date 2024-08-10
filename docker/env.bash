c94278ade0d3d345c53cd9f5a48e3e4ac4c8a0aa
Takashi Mori <kanetugu2015@gmail.com>
2024-08-10 09:24:33 +0900

    add builder

--- docker/env.bash
+++ docker/env.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export HOST_WORKDIR=`pwd`/workspace
-export DOCKER_DIR=/root/workspace
+export HOST_WORKDIR=`pwd`/asp3
+export DOCKER_DIR=/root/asp3
 
 export HOST_ATHRILL_DIR=`pwd`/athrill-target-v850e2m
 export DOCKER_ATHRILL_DIR=/root/athrill-target-v850e2m
