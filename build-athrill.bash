4a44fb14925feb57ba5dbd4aa58986b318d3a257
Takashi Mori <kanetugu2015@gmail.com>
2024-08-11 12:19:23 +0900

    add hakotime for core

--- build-athrill.bash
+++ build-athrill.bash
@@ -16,12 +16,35 @@ then
 else
     mkdir athrill-target-v850e2m/cmake-build
 fi
+CURR_DIR=`pwd`
 cd athrill-target-v850e2m/cmake-build
 cmake ..
 
 make timer32=true serial_fifo_enable=true vdev_disable=true enable_bt_serial=true skip_clock_bugfix=true supress_detect_error=true etrobo_optimize=true reuse_port=true
 
-./athrill2
+cd ${CURR_DIR}
+cd athrill-device/device/hakotime
+export CORE_DIR=${CURR_DIR}/athrill-target-v850e2m/athrill/src
+bash build.bash
+if [ `uname` = "Darwin" ]
+then
+    if [ ! -f cmake-build/libhakotime.dylib ]
+    then
+        echo "ERROR: can not found libhakotime.dylib"
+        exit 1
+    fi
+else
+    if [ ! -f cmake-build/libhakotime.so ]
+    then
+        echo "ERROR: can not found libhakotime.so"
+        exit 1
+    fi
+fi
+echo "now opying libhakotime on /usr/local/lib/hakoniwa, so please enter password:"
+sudo cp cmake-build/libhakotime.* /usr/local/lib/hakoniwa/
+
+cd ${CURR_DIR}
+./athrill-target-v850e2m/cmake-build/athrill2
 if [ $? -eq 0 ]
 then
     echo "OK: athrill2 is created on `pwd`/athrill2"
--- run/exec.bash
+++ run/exec.bash
@@ -14,6 +14,13 @@ then
 fi
 
 cd run
+if [ `uname` = "Darwin" ]
+then
+    MEMORY_CONFIG="memory_mac.txt"
+else
+    MEMORY_CONFIG="memory_lnx.txt"
+fi
+
 ../athrill-target-v850e2m/cmake-build/athrill2 \
-    -c1 -i -m memory.txt -d device_config.txt \
+    -c1 -i -m ${MEMORY_CONFIG} -d device_config.txt \
     ../asp3/${APPNAME}/asp
