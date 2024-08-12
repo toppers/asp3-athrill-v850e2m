4d1b9de0c675ab3b258189919c120d736febbeb7
Takashi Mori <kanetugu2015@gmail.com>
2024-08-12 09:39:37 +0900

    add sample device

--- build-athrill.bash
+++ build-athrill.bash
@@ -22,28 +22,38 @@ cmake ..
 
 make timer32=true serial_fifo_enable=true vdev_disable=true enable_bt_serial=true skip_clock_bugfix=true supress_detect_error=true etrobo_optimize=true reuse_port=true
 
-cd ${CURR_DIR}
-cd athrill-device/device/hakotime
-export CORE_DIR=${CURR_DIR}/athrill-target-v850e2m/athrill/src
-bash build.bash
-if [ `uname` = "Darwin" ]
-then
-    if [ ! -f cmake-build/libhakotime.dylib ]
+function build_device()
+{
+    local devname=$1
+    cd ${CURR_DIR}
+    cd athrill-device/device/${devname}
+    export CORE_DIR=${CURR_DIR}/athrill-target-v850e2m/athrill/src
+    bash build.bash
+    if [ `uname` = "Darwin" ]
     then
-        echo "ERROR: can not found libhakotime.dylib"
-        exit 1
+        if [ ! -f cmake-build/lib${devname}.dylib ]
+        then
+            echo "ERROR: can not found lib${devname}.dylib"
+            exit 1
+        fi
+    else
+        if [ ! -f cmake-build/lib${devname}.so ]
+        then
+            echo "ERROR: can not found lib${devname}.so"
+            exit 1
+        fi
     fi
-else
-    if [ ! -f cmake-build/libhakotime.so ]
-    then
-        echo "ERROR: can not found libhakotime.so"
-        exit 1
-    fi
-fi
-echo "now opying libhakotime on /usr/local/lib/hakoniwa, so please enter password:"
-sudo cp cmake-build/libhakotime.* /usr/local/lib/hakoniwa/
+    echo "now opying lib${devname} on /usr/local/lib/hakoniwa, so please enter password:"
+    sudo cp cmake-build/lib${devname}.* /usr/local/lib/hakoniwa/
 
-cd ${CURR_DIR}
+    cd ${CURR_DIR}
+}
+build_device hakotime
+build_device hakosample
+
+
+
+
 ./athrill-target-v850e2m/cmake-build/athrill2
 if [ $? -eq 0 ]
 then
